6ec54f6c412e69c0325532b8aac0a24d6366d0e7
Ubaswe <127177765+ubaswe@users.noreply.github.com>
2023-12-26 15:03:40 +0600

    Update pandas read csv file.py

--- Pandas -read csv file.py
+++ Pandas -read csv file.py
@@ -1,45 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
+#import pandas and read csv file 
 import pandas as pd
 df=pd.read_csv("data.csv")
 df
 
 
-# In[2]:
-
-
+# In[2]:to print head rows
 df.head()
 
 
-# In[3]:
-
-
+# In[3]:to print tail rows
 df.tail()
 
 
-# In[4]:
-
-
+# In[4]:to print all columns
 df.columns
 
 
-# In[5]:
-
-
+# In[5]:to print specific column
 df.Day
 
 
-# In[7]:
-
-
+# In[7]:to print specific column
 df.Event
 
-
-# In[ ]:
 
 
 
