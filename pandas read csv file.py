607b61e575423e2e6899550711fbf7378f058b8d
Ubaswe <127177765+ubaswe@users.noreply.github.com>
2023-12-26 17:23:25 +0600

    Different Ways of Creating DataFram.py

--- pandas read csv file.py
+++ pandas read csv file.py
@@ -3,6 +3,25 @@ import pandas as pd
 df=pd.read_csv("data.csv")
 df
 
+#import pandas and read excel file
+import pandas as pd
+df=pd.read_excel("climate.xlsx","Sheet1")
+df
+
+#import pandas and read dictionary file
+climate_data ={
+    "Day":["1/1/2023","1/2/2023","1/3/2023","1/4/2023"],
+    "Temparature":[35,36,37,38],
+    "Winspeed":[3,6,9,7],
+    "Event":["rainy","sunny","rainy","sunny"] 
+}
+df=pd.DataFrame(climate_data)
+df
+
+
+
+
+
 
 # In[2]:to print head rows
 df.head()
