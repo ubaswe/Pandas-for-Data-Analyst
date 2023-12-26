bcc4c6af883162494f01f31613235e6fc500aadd
Ubaswe <127177765+ubaswe@users.noreply.github.com>
2023-12-26 14:53:27 +0600

    Add files via upload

---
+++ Pandas -read csv file.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+df=pd.read_csv("data.csv")
+df
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[3]:
+
+
+df.tail()
+
+
+# In[4]:
+
+
+df.columns
+
+
+# In[5]:
+
+
+df.Day
+
+
+# In[7]:
+
+
+df.Event
+
+
+# In[ ]:
+
+
+
+
